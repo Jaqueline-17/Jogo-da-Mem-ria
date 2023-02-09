5d175d48be0a3424639612af406fbb36daba40cc
Jaqueline Lessa <jaqueline_lessa@outlook.com>
2023-02-08 21:59:15 -0300

    Inclusão de Projeto

---
+++ Script/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login_form');
+
+const validarInput = ({ target }) => {
+    if(target.value.length > 2){
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', '');
+    }
+}
+
+const enviarNome = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = '../pages/game.html';
+}
+
+input.addEventListener('input', validarInput);
+form.addEventListener('submit', enviarNome);
